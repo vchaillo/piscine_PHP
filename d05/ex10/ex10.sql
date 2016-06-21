cbe7866903eaa517dbda213ac017027346988410
Valentin CHAILLOU <vchaillo@e2r4p5.42.fr>
2016-06-21 23:42:07 +0200

    update ex10 d05

--- d05/ex10/ex10.sql
+++ d05/ex10/ex10.sql
@@ -1,1 +1,4 @@
-SELECT titre as "Titre", resum as "Resume", annee_prod FROM film WHERE id_genre = '25' ORDER BY annee_prod DESC;
+SELECT `titre` AS 'Titre', `resum` AS 'Resume', `annee_prod`
+	FROM `db_vchaillo`.`film`
+	INNER JOIN `db_vchaillo`.`genre` ON `film`.`id_genre` = `genre`.`id_genre`
+	WHERE `genre`.`nom` = 'erotic'
